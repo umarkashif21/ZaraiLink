fd8e00edd94f7a6b384b44d349311ff0acfce4db
Salman Adnan <grmculmfuer9@gmail.com>
2025-12-03 10:17:40 +0500

    Resources

---
+++ frontend/src/components/TradeIntelligence/TradeLens.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Layout/Navbar';
+import './TradeIntelligence.css';
+
+const TradeLens = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="placeholder-container">
+        <h1 className="placeholder-title">Trade Lens, coming soon...</h1>
+      </div>
+    </>
+  );
+};
+
+export default TradeLens;
---
+++ frontend/src/components/TradeIntelligence/TradePulse.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Layout/Navbar';
+import './TradeIntelligence.css';
+
+const TradePulse = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="placeholder-container">
+        <h1 className="placeholder-title">Trade Pulse, coming soon...</h1>
+      </div>
+    </>
+  );
+};
+
+export default TradePulse;
